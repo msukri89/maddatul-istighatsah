954a75b5049fd3131e744a0dd614995c17894370
msukri89 <mochammadsyukri@gmail.com>
2026-04-30 12:06:27 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maddatul-istighatsah-v3'; // Versi 3 untuk memaksa update
+const CACHE_NAME = 'maddatul-istighatsah-v4'; // Versi 3 untuk memaksa update
 const urlsToCache = [
   './',
   './index.html',
